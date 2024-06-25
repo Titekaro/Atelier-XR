3caa8c3958c1a26ef2fa22965afbeffd563985b0
Caroline Pierson <caroline-pierson@live.be>
2024-06-25 21:29:21 +0200

    :hatching_chick: Allow to start Univac only when every socket are filled

--- Assets/Scripts/Univac Manager.cs
+++ Assets/Scripts/Univac Manager.cs
@@ -6,6 +6,11 @@ public class UnivacManager : MonoBehaviour
 {
     private Player playerScript;
 
+    [SerializeField] private GameObject bobine;
+    [SerializeField] private GameObject perforedCards;
+    [SerializeField] private GameObject pressCards;
+    [SerializeField] private GameObject buttonOn;
+
     void Awake() {
         playerScript = GameObject.Find("Scripts Access").GetComponent<Player>();
         playerScript.ResetPlayerPosition(new Vector3(-7.97f,0f,3.82f));
@@ -16,6 +21,14 @@ public class UnivacManager : MonoBehaviour
     }
 
     void Update() {
-        
+        RunUnivac();
+    }
+
+    void RunUnivac() {
+        if(bobine.tag == "Set" && perforedCards.tag == "Set" && pressCards.tag == "Set") {
+            buttonOn.SetActive(true);
+        } else {
+            buttonOn.SetActive(false);
+        }
     }
 }
