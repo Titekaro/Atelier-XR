330995c1d35557c0fad4ed2a403697443da0ec51
Caroline Pierson <Titekaro@users.noreply.github.com>
2024-06-25 16:34:51 +0200

    haptic

--- Assets/Scripts/Hand Scan.cs
+++ Assets/Scripts/Hand Scan.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class HandScan : MonoBehaviour
 {
     private ScenesManager scenesManagerScript;
+    private XRDirectInteractor hand;
 
     void Awake() {
         scenesManagerScript = GameObject.Find("Scripts Access").GetComponent<ScenesManager>(); // Access the wanted script in "Scripts Access"
+        hand = GameObject.Find("Left Controller").GetComponent<XRDirectInteractor>();
     }
 
     void Start() {  
@@ -17,9 +22,25 @@ public class HandScan : MonoBehaviour
     void Update() {
     }
 
-    void OnTriggerEnter() {
-        Debug.Log("collision");
-        scenesManagerScript.SwitchScene(3);
+    void OnTriggerEnter(Collider other){
+        //Ajouter detection uniquement quand c'est les mains
+        Debug.Log("collision " + other.tag);
+
+        if(other.tag == "Hand") {
+            XRBaseController leftHandController = GameObject.Find("Left Controller").GetComponent<XRBaseController>();
+            if (leftHandController != null) leftHandController.SendHapticImpulse(0.5f, 0.5f);
+            
+            StartCoroutine(DelayAction(5));
+        }
     }
 
+    void OnTriggerExit() {
+        hand.playHapticsOnHoverEntered = false;
+    }
+    
+    IEnumerator DelayAction(float delayTime) {
+        //Wait for the specified delay time before continuing.
+        yield return new WaitForSeconds(delayTime);
+        scenesManagerScript.SwitchScene(3);
+    }
 }
