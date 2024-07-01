0ffcabc79ddcca77246ed22557f0a782cf2401fe
Caroline Pierson <caroline-pierson@live.be>
2024-07-01 22:25:30 +0200

    :hatching_chick: Implement the collect of code in player wrist

--- Assets/Scripts/IA Manager.cs
+++ Assets/Scripts/IA Manager.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class IAManager : MonoBehaviour
 {
     private Player playerScript;
+    private PlayerWristManager wristManagerScript;
 
     [SerializeField] private GameObject playerPositionResetter;
 
     void Awake() {
         playerScript = GameObject.Find("Scripts Access").GetComponent<Player>();
+        wristManagerScript = GameObject.Find("Wrist").GetComponent<PlayerWristManager>();
     }
 
     void Start() {
         playerScript.ResetPlayerPosition(playerPositionResetter);
+
+        if (wristManagerScript.totalCodeFound == wristManagerScript.totalCodeToFind) {
+            // Show wrist trigger
+        }
     }
 
     void Update() {
