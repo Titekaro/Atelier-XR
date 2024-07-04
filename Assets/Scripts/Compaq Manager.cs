b56d28e0f4a1ebdd8816c8b75137d237a03813c9
Caroline Pierson <Titekaro@users.noreply.github.com>
2024-07-04 13:58:29 +0200

    :ok_hand: Refactor Compaq & Macintosh Manager scripts + add method onCodeFound

--- Assets/Scripts/Compaq Manager.cs
+++ Assets/Scripts/Compaq Manager.cs
@@ -1,25 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class CompaqManager : MonoBehaviour
 {
     private Player playerScript;
+    private ScenesManager scenesManagerScript;
+    private PlayerWristManager wristManagerScript;
+    private GameObject leftHand;
 
     [SerializeField] private GameObject playerPositionResetter;
 
     void Awake() {
+        scenesManagerScript = GameObject.Find("Scripts Access").GetComponent<ScenesManager>(); // Access the wanted script in "Scripts Access"
         playerScript = GameObject.Find("Scripts Access").GetComponent<Player>();
-        playerScript.ResetPlayerPosition(playerPositionResetter);
+
+        leftHand = GameObject.Find("Left Controller");
     }
 
-    void Start() {
-        
+    void Start() { 
+        playerScript.ResetPlayerPosition(playerPositionResetter); 
     }
 
     void Update() {
-    
     }
 
+    // Method called when the wrist zone is triggered with the player wrist
+    public void OnCodeFound() {
+        wristManagerScript = GameObject.Find("Wrist").GetComponent<PlayerWristManager>();
+        wristManagerScript.CollectCode("Compaq");
+        
+        // Add haptic impulse
+        XRBaseController leftHandController = leftHand.GetComponent<XRBaseController>();
+
+        if(leftHandController != null) {
+            leftHandController.SendHapticImpulse(0.5f, 0.5f);
+        }
+
+        // Call switch scene method with delay
+        StartCoroutine(DelayAction(1));
+    }
+
+    IEnumerator DelayAction(float delayTime) {
+        //Wait for the specified delay time before continuing.
+        yield return new WaitForSeconds(delayTime);
+        scenesManagerScript.SwitchScene(2);
+    }
 
 }
--- Assets/Scripts/Macintosh Manager.cs
+++ Assets/Scripts/Macintosh Manager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class MacintoshManager : MonoBehaviour
 {
     private Player playerScript;
+    private ScenesManager scenesManagerScript;
+    private PlayerWristManager wristManagerScript;
+    private GameObject leftHand;
 
     [SerializeField] private GameObject playerPositionResetter;
     [SerializeField] private GameObject Mouse;
@@ -12,18 +16,39 @@ public class MacintoshManager : MonoBehaviour
     [SerializeField] private GameObject Sol;
 
     void Awake() {
+        scenesManagerScript = GameObject.Find("Scripts Access").GetComponent<ScenesManager>(); // Access the wanted script in "Scripts Access"
         playerScript = GameObject.Find("Scripts Access").GetComponent<Player>();
-        playerScript.ResetPlayerPosition(playerPositionResetter);
+
+        leftHand = GameObject.Find("Left Controller");
     }
 
     void Start() {
-        
+        playerScript.ResetPlayerPosition(playerPositionResetter);
     }
 
     void Update() {
     }
 
-    void RunUnivac() {
+    // Method called when the wrist zone is triggered with the player wrist
+    public void OnCodeFound() {
+        wristManagerScript = GameObject.Find("Wrist").GetComponent<PlayerWristManager>();
+        wristManagerScript.CollectCode("Macintosh");
+        
+        // Add haptic impulse
+        XRBaseController leftHandController = leftHand.GetComponent<XRBaseController>();
+
+        if(leftHandController != null) {
+            leftHandController.SendHapticImpulse(0.5f, 0.5f);
         }
+
+        // Call switch scene method with delay
+        StartCoroutine(DelayAction(1));
+    }
+
+    IEnumerator DelayAction(float delayTime) {
+        //Wait for the specified delay time before continuing.
+        yield return new WaitForSeconds(delayTime);
+        scenesManagerScript.SwitchScene(5);
+    }
     
 }
